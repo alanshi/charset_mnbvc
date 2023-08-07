b25077264ff06ea0176ec9ecb45b0adb28a1818e
alan <alan.shi86@gmail.com>
2023-08-07 11:16:40 +0800

    fix file_path

--- examples/check_disorder_chars_example.py
+++ examples/check_disorder_chars_example.py
@@ -9,7 +9,7 @@ def check_disorder_chars():
     """
     check the disorder chars
     """
-    file_path = "/Users/alan/mywork/mnbvc/tests/fixtures/errors/48.txt"
+    file_path = "tests/fixtures/test5.txt"
     ret, ratio = api.check_disorder_chars(file_path=file_path, threshold=0.05)
     print(f"包含乱码的字符拷锟斤等字符, 乱码比例约:{round(float(ratio)*100)}%" if ret else "未找到乱码的字符，请注意调节阈值")
 
