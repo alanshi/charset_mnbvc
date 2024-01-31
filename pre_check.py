0934d516ecb65cadabec260ac716afb8b1d57097
alan <alan.shi86@gmail.com>
2024-01-31 11:45:10 +0800

    imporve pre_check.py

--- pre_check.py
+++ pre_check.py
@@ -1,4 +1,7 @@
+import csv
+import time
 import argparse
+
 
 from concurrent.futures import ProcessPoolExecutor
 from pathlib import Path
@@ -7,6 +10,7 @@ from tqdm import tqdm
 
 from charset_mnbvc import api, verify
 from charset_mnbvc.common_utils import get_file_paths
+
 
 def parse_args():
     """
@@ -37,6 +41,14 @@ def parse_args():
         dest='folder_path',
         help='inputDirectory为需要检测的目录'
     )
+    parser.add_argument(
+        '-r',
+        required=False,
+        default=f'check_result_{int(time.time())}.csv',
+        metavar='check_result_file_name',
+        dest='check_result_file_name',
+        help='指定编码检测结果文件名'
+    )
     return parser.parse_args()
 
 
@@ -44,16 +56,12 @@ def encoding_check(inputs):
     """
     编码检测
     """
-    # file_count, files = api.from_dir(
-    #     folder_path=inputs.folder_path,
-    #     mode=inputs.mode
-    # )
     files = []
     results = []
     files = get_file_paths(inputs.folder_path, suffix='.txt')
     with ProcessPoolExecutor(inputs.process_num) as executor:
         futures = []
-        with tqdm(desc="检测进度", total=len(files)) as pbar:
+        with tqdm(desc="检测进度1", total=len(files)) as pbar:
             # 提交任务，并将Future对象添加到futures列表中
             for file in files:
                 future = executor.submit(api.from_file, file, inputs.mode)
@@ -66,25 +74,35 @@ def encoding_check(inputs):
 
     return results
 
+
 def convert_check(file_path, encoding):
+    result = {
+        "file_path": file_path,
+        "status": True,
+        "msg": "success"
+    }
     with open(file_path, "rb") as f:
         data = f.read()
         if not data:
-            return False, f"{file_path}, 文件为空"
+            result['status'] = False
+            result['msg'] = "文件为空"
         try:
-            ret = api.decode_check(data, encoding)
-            if not ret[0]:
-                return False, f"{file_path}, {ret[1]}"
+            ret = api.decode(data, encoding)
+            if not ret:
+                result['status'] = False
+                result['msg'] = "文件为空"
         except Exception as e:
-            return False, f"{file_path}, {str(e)}"
+            result['status'] = False
+            result['msg'] = str(e)
 
-    return True, "success"
+    return result
+
 
 def process(files, inputs):
     results = []
     with ProcessPoolExecutor(inputs.process_num) as executor:
         futures = []
-        with tqdm(desc="检测进度", total=len(files)) as pbar:
+        with tqdm(desc="检测进度2", total=len(files)) as pbar:
             for file in files:
                 file_path = file[0]
                 encoding = file[1]
@@ -101,15 +119,14 @@ def process(files, inputs):
 
 def main():
     inputs = parse_args()
-    input_folder = inputs.folder_path
-    mode = inputs.mode
-    process_num = inputs.process_num
     encoding_results = encoding_check(inputs)
     process_results = process(encoding_results, inputs)
-    for i in process_results:
-        if not i[0]:
-            print(i)
-
+    with open(inputs.check_result_file_name, 'w', newline='') as file:
+        writer = csv.writer(file)
+        for row in process_results:
+            if not row['status']:
+                writer.writerow([row['file_path'], row['msg']])
+    print(f"已将检测出错结果保存至 {inputs.check_result_file_name} 文件中,请查阅!")
 
 
 if __name__ == "__main__":
